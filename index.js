5b4f88801106346cea83e9988e5bf8c3ad9695e4
Akash Bhatthal <akashbhatthal@gmail.com>
2024-05-05 12:42:20 -0700

    Change default port, update to latest API version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5555;
 
 const cool = require('cool-ascii-faces');
 
